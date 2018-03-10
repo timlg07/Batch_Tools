2f2acd541ee2d0f7cac9536ccd6e7cf45edefebf
T!M Greller <mail@tim-greller.ml>
2018-03-10 15:58:23 +0100

    1-1-int2binary -> fixed leading zero

--- 1-1-int2binary.bat
+++ 1-1-int2binary.bat
@@ -1,7 +1,8 @@
 @echo off & setlocal enabledelayedexpansion
 set "result=" & set /a this=0 & set /a next=1
 set /p "Zahl[0]=Zahl: >"
-set /a Zahl[0]+=0
+call :deleteLeadingZero %Zahl[0]%
+set /a Zahl[0]=%errorlevel%
 :while
 set /a Zahl[%next%]=!Zahl[%this%]!/2
 set /a binary[%this%]=!Zahl[%this%]!%%2
@@ -11,3 +12,5 @@ set /a next+=1
 if !Zahl[%this%]! NEQ 0 goto while
 echo %Zahl[0]% in binaer: %result%
 echo. & %0
+:deleteLeadingZero
+exit /B %~1
