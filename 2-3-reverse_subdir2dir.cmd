58f437f4265d298d728655d44ba6e8bcdf44a98d
T!M Greller <timlg07@gmail.com>
2018-06-02 11:28:32 +0200

    2-3 -> changes.log reading fixed
    multiple character delim isn't supported by the FOR /F loop and caused problems when source or destination(=target) contained a character from the delims $MOVED_TO;  Each line of the changes.log is now split by replacing the delimeter with a set command.

--- 2-3-reverse_subdir2dir.cmd
+++ 2-3-reverse_subdir2dir.cmd
@@ -1,16 +1,17 @@
 color 0a & title reversing subdir 2 dir
-for /F "tokens=1,2 delims=$MOVED_TO;" %%s in (changes.log) do (
-    set "source=%%~s"
-    set "target=%%~t"
-    call :reverse
+for /F "delims=" %%l in (changes.log) do (
+    call :reverse "%%l"
 )
 del changes.log
 exit
 
 
 :reverse
-for /F "tokens=1 delims=\" %%D in ("%source%") do (
-    if not exist "%%D" mkdir "%%D"
-)
-MOVE /-Y "%target%" "%source%"
+    set "line=%~1"
+    set "source=%line:$MOVED_TO;=" & set "target=%"
+
+    for /F "tokens=1 delims=\" %%D in (%source%) do (
+        if not exist %%D mkdir %%D
+    )
+    MOVE /-Y %target% %source%
 exit /B
