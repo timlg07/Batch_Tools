6fed884d700d24f6fbc601cf10c77babe5ba696f
T!M Greller <timlg07@gmail.com>
2019-01-30 17:31:29 +0100

    added my default function macros and used them

--- 3-4-formatNumber.cmd
+++ 3-4-formatNumber.cmd
@@ -1,14 +1,20 @@
-@echo off & setlocal EnableDelayedExpansion
+@echo off
+:: //====// INIT MAKROS //====// ::
+set "DelayedExpansion{=(setlocal enableDelayedExpansion&for /F "tokens=1* delims==" %%V in ('set @') do set "%%V=")>nul 2>&1"
+set "{=(setlocal&for /F "tokens=1* delims==" %%V in ('set @') do set "%%V=")>nul 2>&1"
+set "}=(for /F "tokens=1* delims==" %%V in ('set $^&set @') do endlocal&set "%%V=%%W")>nul 2>&1&exit/B "
+:: //=========================// ::
 
-call :formatNumberLength  2 10 " "
+call :formatNumberLength  2 10 "."
 echo %@return%
 call :formatNumberLength 34 10 "."
 echo %@return%
-call :formatNumberLength 987 10 " "
+call :formatNumberLength 987 10 "."
 echo %@return%
 pause&exit
 
 :formatNumberLength <Number> <length> [<filler>="0"]
+%DelayedExpansion{%
     set "@return=%~1"
     set "_f=0"
     if not "%~3"=="" set "_f=%~3"
@@ -16,4 +22,4 @@ pause&exit
         set "@return=%_f%!@return!"
     )
     set "@return=!@return:~-%~2!"
-exit /B
+%}%
