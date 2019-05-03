15dfa29338d6c43863990fc38c489c0478d9bcdc
T!M Greller <timlg07@gmail.com>
2019-05-03 16:22:44 +0200

    [1.1] more precise naming

--- 1-1-int2binary.bat
+++ 1-1-int2binary.bat
@@ -1,17 +1,17 @@
 @echo off
 :main
     set "result="
-    set /p "num_0=Zahl: >"
+    set /p "num_0=Dezimalzahl: >"
 
     call :deleteLeadingZero %num_0%
     set /a num_0  = %errorlevel%
     set /a number = %num_0%
 
-    :while
+    :do_while
         set /a number = %number% /  2
         set /a binary = %number% %% 2
         set   "result=%binary%%result%"
-    if %number% NEQ 0 goto while
+    if %number% NEQ 0 goto do_while
 
     echo %num_0% in binaer: %result% &echo+
 goto main
