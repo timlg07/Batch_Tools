192bb4153cc5d83c17a2b477521c482112dbd4bf
T!M Greller <timlg07@gmail.com>
2019-10-30 18:27:45 +0100

    [1.1] added support for negative numbers

--- 1-1-int2binary.bat
+++ 1-1-int2binary.bat
@@ -1,17 +1,34 @@
 @echo off
 :main
     set "result="
+    set "negative=0"
     set /p "num_0=Dezimalzahl: >"
 
     call :deleteLeadingZero %num_0%
     set /a num_0  = %errorlevel%
     set /a number = %num_0%
 
+    if %number% lss 0 (
+        set "negative=1"
+        set /a number *= -1
+        set /a number -= 1
+    )
+
     :do_while
-        set /a number = %number% /  2
         set /a binary = %number% %% 2
-        set   "result=%binary%%result%"
+        set /a number = %number% /  2
+        if %negative% equ 1 (
+            if %binary% equ 0 (
+                set /a binary = 1
+            ) else (
+                set /a binary = 0
+            )
+        )
+        set "result=%binary%%result%"
     if %number% NEQ 0 goto do_while
+
+set "result=%negative%%result%"
+
 
     echo %num_0% in binaer: %result% &echo+
 goto main
